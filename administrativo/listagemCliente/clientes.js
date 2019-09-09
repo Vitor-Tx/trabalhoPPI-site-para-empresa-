bb1211dba57c4bbfe206dc35b9ab0a9a7261a451
mobileReptile <guilhermebnj@gmail.com>
2019-09-09 11:38:34 -0300

    Listagem de Funcionários e de Clientes

---
+++ administrativo/listagemCliente/clientes.js
@@ -0,0 +1,22 @@
+var clientes = [
+    {
+        "nome": "Guilherme Bartasson",
+        "cpf": "137.682.906-11",
+        "endereco": "Rua Guaporé 526, Bairro Santa Rosa, Uberlândia - MG",
+        "telefones": ["(34)99999-9999", "(34)88888-8888", "(34)77777-7777"],
+        "email": "guilhermebnj@gmail.com",
+        "sexo": "Masculino",
+        "estadoCivil": "Solteiro",
+        "profissão": "Analista de Automação"
+    },
+    {
+        "nome": "Sebastião Mota",
+        "cpf": "987.654.321-87",
+        "endereco": "Rua A 123, Bairro Estrela, Passo Fundo - AC",
+        "telefones": ["(24)22222-2222", "(64)66666-6666"],
+        "email": "sebastiaom@hotmail.com",
+        "sexo": "Masculino",
+        "estadoCivil": "Casaddo",
+        "profissão": "Agricultor"
+    }
+];
